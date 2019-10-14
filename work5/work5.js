e8000300c8ae092d73eb92a5c2aece404e35359d
zhangzhanhua1229 <1754164648@qq.com>
2019-10-14 14:57:17 +0800

    jswork

---
+++ work5/work5.js
@@ -0,0 +1,13 @@
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str += '<td>' + j + '*' + i + '=' + (j * i ) + '</td>'
+    }
+    str +='</tr>'
+}
+str +='</table>'
+document.getElementById('table2').innerHTML = str
+
+
+
